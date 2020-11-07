d1c3b0aa25db92d4a0e335af851d6d837bfc431f
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2020-11-08 00:10:18 +0700

    chore: Add google analytics

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,6 +20,17 @@
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
   @stack('stylesheets')
+
+  @if(env('GOOGLE_ANALYTICS_ID'))
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
+    </script>
+  @endif
 </head>
 <body>
   <div id="app">
