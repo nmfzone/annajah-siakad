c7dc53e61ac1beecac3ae9ef1054ca3098beef10
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2020-12-24 22:36:02 +0700

    feature(queue): Run queue processor via scheduler

--- routes/schedule.php
+++ routes/schedule.php
@@ -3,3 +3,19 @@
 /** @var \Illuminate\Console\Scheduling\Schedule $schedule */
 
 $schedule->command('telescope:prune --hours=48')->daily();
+
+$isCommandRunning = function ($command) {
+    exec('ps aux -ww', $process_status);
+
+    $result = array_filter($process_status, function ($var) use ($command) {
+        return strpos($var, $command);
+    });
+
+    return count($result) > 0;
+};
+
+$command = 'queue:work --tries=3 --queue=high,medium,low,default';
+if (! $isCommandRunning($command)) {
+    $schedule->command($command)
+        ->everyMinute();
+}
