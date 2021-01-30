c5d8404549bd14e9c09cc2296b31efc1a049368e
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2021-01-30 12:47:09 +0700

    chore: Add DatePicker test component

---
+++ tests/Browser/Components/DatePicker.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Tests\Browser\Components;
+
+use Facebook\WebDriver\Remote\RemoteWebElement;
+use Facebook\WebDriver\WebDriverBy;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Component as BaseComponent;
+use PHPUnit\Framework\Assert as PHPUnit;
+use Tests\Concerns\Helper;
+
+class DatePicker extends BaseComponent
+{
+    use Helper;
+
+    protected $baseSelector;
+
+    protected $browser;
+
+    protected $invalidYear = 'The choosen year is not available.';
+
+    protected $invalidMonth = 'The choosen month is not available.';
+
+    protected $invalidDate = 'The choosen date is not available.';
+
+    public function __construct(string $baseSelector)
+    {
+        $this->baseSelector = $baseSelector;
+    }
+
+    public function selector(): string
+    {
+        return 'body';
+    }
+
+    public function assert(Browser $browser): void
+    {
+        PHPUnit::assertTrue(
+            $this->getDatePickerElement($browser)->isDisplayed(),
+            "Datepicker for ['{$this->baseSelector}'] is not visible."
+        );
+    }
+
+    public function elements(): array
+    {
+        return [
+            '@element' => '#selector',
+        ];
+    }
+
+    public function selectDate(Browser $browser, int $year, int $month, int $day)
+    {
+        $pickerEl = $this->getDatePickerElement($browser);
+        $this->pickerSelectYear($pickerEl, $year);
+        $this->pause(100);
+        $this->pickerSelectMonth($pickerEl, $month);
+        $this->pause(100);
+        $this->pickerSelectDate($pickerEl, $day);
+        $this->pause(100);
+    }
+
+    protected function pickerSelectDate(RemoteWebElement $pickerEl, int $date)
+    {
+        $dateEl = $this->getDateElement($pickerEl, $date);
+
+        PHPUnit::assertTrue(
+            ! is_null($dateEl) && ! Str::contains(
+                $dateEl->getAttribute('class'),
+                'cursor-not-allowed'
+            ),
+            $this->invalidDate
+        );
+
+        $dateEl->click();
+    }
+
+    protected function getDateElement(RemoteWebElement $pickerEl, int $date):  ?RemoteWebElement
+    {
+        $elements = $pickerEl->findElements(
+            WebDriverBy::cssSelector('.picker-dates .picker-date')
+        );
+
+        foreach ($elements as $element) {
+            if ($element->getText() == $date) {
+                return $element;
+            }
+        }
+
+        return null;
+    }
+
+    protected function pickerSelectMonth(RemoteWebElement $pickerEl, int $month)
+    {
+        $toggleMonthEl = $pickerEl
+            ->findElement(
+                WebDriverBy::cssSelector('.picker-toggle-month')
+            );
+
+        if ($this->convertMonthNameToNumber($toggleMonthEl->getText()) == $month) {
+            return;
+        }
+
+        $toggleMonthEl->click();
+        $this->pause(100);
+
+        $monthEl = $this->getMonthElement($pickerEl, $month);
+
+        PHPUnit::assertTrue(
+            ! is_null($monthEl) && ! Str::contains(
+                $monthEl->getAttribute('class'),
+                'cursor-not-allowed'
+            ),
+            $this->invalidMonth
+        );
+
+        $monthEl->click();
+    }
+
+    protected function getMonthElement(RemoteWebElement $pickerEl, int $month):  ?RemoteWebElement
+    {
+        $elements = $pickerEl->findElements(
+            WebDriverBy::cssSelector('.picker-months .picker-month')
+        );
+
+        foreach ($elements as $element) {
+            $text = $element->getText();
+            if (! empty($text) && $this->convertMonthNameToNumber($text) == $month) {
+                return $element;
+            }
+        }
+
+        return null;
+    }
+
+    protected function convertMonthNameToNumber($name): int
+    {
+        return Carbon::parse(
+            Carbon::translateTimeString('1 ' . $name, Carbon::getLocale(), 'en')
+        )->month;
+    }
+
+    protected function pickerSelectYear(RemoteWebElement $pickerEl, int $year)
+    {
+        $toggleYearEl = $pickerEl
+            ->findElement(
+                WebDriverBy::cssSelector('.picker-toggle-year')
+            );
+
+        if ($toggleYearEl->getText() == $year) {
+            return;
+        }
+
+        $toggleYearEl->click();
+        $this->pause(100);
+
+        $yearEl = null;
+        while ($yearEl == null) {
+            $years = $this->getPickerYears($pickerEl, true);
+
+            PHPUnit::assertTrue(
+                count($years) > 0,
+                'Year elements not available.'
+            );
+
+            if (! in_array($year, $years)) {
+                if ($years[0] > $year) {
+                    $this->moveToPreviousOrNext($pickerEl);
+                } else if (last($years) < $year) {
+                    $this->moveToPreviousOrNext($pickerEl, false);
+                }
+                $this->pause(100);
+            } else {
+                $yearEl = $this->getYearElement($pickerEl, $year);
+            }
+        }
+
+        PHPUnit::assertFalse(
+            Str::contains(
+                $yearEl->getAttribute('class'),
+                'cursor-not-allowed'
+            ),
+            $this->invalidYear
+        );
+
+        $yearEl->click();
+    }
+
+    protected function moveToPreviousOrNext(RemoteWebElement $pickerEl, $toPrevious = true)
+    {
+        $buttonEl = $pickerEl
+            ->findElement(
+                WebDriverBy::cssSelector(
+                    $toPrevious
+                        ? '.picker-previous'
+                        : '.picker-next'
+                )
+            );
+
+        PHPUnit::assertFalse(
+            Str::contains(
+                $buttonEl->getAttribute('class'),
+                'cursor-not-allowed'
+            ),
+            $this->invalidYear
+        );
+
+        $buttonEl->click();
+    }
+
+    protected function getYearElement(RemoteWebElement $pickerEl, int $year): ?RemoteWebElement
+    {
+        $elements = $this->getPickerYears($pickerEl);
+
+        foreach ($elements as $element) {
+            if ($element->getText() == $year) {
+                return $element;
+            }
+        }
+
+        return null;
+    }
+
+    protected function getPickerYears(RemoteWebElement $pickerEl, $asNumber = false): array
+    {
+        $elements = $pickerEl->findElements(
+            WebDriverBy::cssSelector('.picker-years .picker-year')
+        );
+
+        $years = [];
+        foreach ($elements as $element) {
+            $years[] = $asNumber ? (int) $element->getText() : $element;
+        }
+
+        if ($asNumber) {
+            sort($years);
+        }
+
+        return $years;
+    }
+
+    protected function getDatePickerElement(Browser $browser): ?RemoteWebElement
+    {
+        $targetEl = $browser->resolver->findOrFail($this->baseSelector);
+        $parentEl = $targetEl->findElement(WebDriverBy::xpath('./../..'));
+
+        return $parentEl->findElement(
+            WebDriverBy::id('v-tailwind-picker')
+        );
+    }
+}
