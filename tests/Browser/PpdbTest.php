9fc8ef8175df8e696f008a48b14579f4015dc6d6
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2021-01-30 12:47:43 +0700

    chore: Finish create ppdb test

--- tests/Browser/PpdbTest.php
+++ tests/Browser/PpdbTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use App\Models\AcademicYear;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
+use Tests\Browser\Components\DatePicker;
 use Tests\Browser\Components\VueFormSelect;
 use Tests\Browser\Pages\CreatePpdbPage;
 use Tests\Concerns\CreatesSite;
@@ -52,7 +53,29 @@ class PpdbTest extends DuskTestCase
                 ->within($academicYearComponent, function (Browser $browser) {
                     $browser->selectValue('2020/2021');
                 })
-                ->screenshot('coba');
+                ->click('#start_date')
+                ->pause(200)
+                ->within(new DatePicker('#start_date'), function (Browser $browser) {
+                    $browser->selectDate(1921, 2, 20);
+                })
+                ->type('#start_time', '20:30')
+                ->click('#end_date')
+                ->pause(200)
+                ->within(new DatePicker('#end_date'), function (Browser $browser) {
+                    $browser->selectDate(2020, 2, 20);
+                })
+                ->type('#end_time', '11:30')
+                ->type('#price', 100000)
+                ->select('#payment_provider', 'mandiri')
+                ->select('#payment_type', 'bank_transfer')
+                ->type('#payment_provider_number', '0512839301828')
+                ->type('#payment_provider_holder_name', 'John Doe')
+                ->type('#contact_persons_name_0', 'Jane Doe')
+                ->type('#contact_persons_number_0', '081328019287')
+                ->press('Simpan')
+                ->assertHostIs($this->site->domain)
+                ->assertPathIs('/backoffice/ppdb')
+                ->assertSee('Berhasil menambahkan PPDB.');
         });
     }
 }
