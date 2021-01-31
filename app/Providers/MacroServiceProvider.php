0a32762faa3268125bccdcb78917ac42b654ab39
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2021-02-01 06:50:32 +0700

    chore: Add dockblock to the macro

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -35,20 +35,28 @@ class MacroServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Arr::macro('implodeKeys', function (array $array, $glue = ',') {
+        /**
+         * Implode array keys.
+         *
+         * @param  array  $array
+         * @param  string  $glue
+         * @return string
+         */
+        Arr::macro('implodeKeys', function (array $array, string $glue = ',') {
             return implode($glue, array_keys($array));
         });
 
-        /*
+        /**
          * Deep merge array.
          * Combine (instead of replace) array with the same key (if the value is an array).
          *
+         * @param  mixed  $args,...
          * @return array
          */
-        Arr::macro('mergeDeep', function () {
+        Arr::macro('mergeDeep', function (...$args) {
             $result = [];
 
-            foreach (func_get_args() as $array) {
+            foreach ($args as $array) {
                 foreach (Arr::wrap($array) as $key => $value) {
                     if (is_integer($key)) {
                         $result[] = $value;
@@ -63,38 +71,73 @@ class MacroServiceProvider extends ServiceProvider
             return $result;
         });
 
+        /**
+         * Deep merge array to the collection.
+         *
+         * @param  mixed  $items
+         * @return \Illuminate\Support\Collection
+         */
         Collection::macro('mergeDeep', function ($items) {
             return new static(Arr::mergeDeep($this->items, $this->getArrayableItems($items)));
         });
 
-        Str::macro('randomPlus', function ($type = 'alnum', $length = 8, $lowerOnly = false) {
-            if ($type == 'alnum') {
-                $pool = '0123456789abcdefghijklmnopqrstuvwxyz';
+        /**
+         * Generate a random string.
+         *
+         * @param  string  $type
+         * @param  int  $length
+         * @param  bool  $lowerOnly
+         * @return string
+         */
+        Str::macro(
+            'randomPlus',
+            function (string $type = 'alnum', int $length = 8, bool $lowerOnly = false) {
+                if ($type == 'alnum') {
+                    $pool = '0123456789abcdefghijklmnopqrstuvwxyz';
 
-                if (! $lowerOnly) {
-                    $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-                }
-            } elseif ($type == 'alpha') {
-                $pool = 'abcdefghijklmnopqrstuvwxyz';
+                    if (! $lowerOnly) {
+                        $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    }
+                } elseif ($type == 'alpha') {
+                    $pool = 'abcdefghijklmnopqrstuvwxyz';
 
-                if (! $lowerOnly) {
-                    $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    if (! $lowerOnly) {
+                        $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    }
+                } elseif ($type == 'numeric') {
+                    $pool = '0123456789';
+                } elseif ($type == 'nozero') {
+                    $pool = '123456789';
+                } else {
+                    throw new Exception("Type '{$type}' is not supported.");
                 }
-            } elseif ($type == 'numeric') {
-                $pool = '0123456789';
-            } elseif ($type == 'nozero') {
-                $pool = '123456789';
-            } else {
-                throw new Exception("Type '{$type}' is not supported.");
+
+                return substr(str_shuffle(
+                    str_repeat($pool, (int) ceil($length / strlen($pool)))
+                ), 0, $length);
             }
+        );
 
-            return substr(str_shuffle(
-                str_repeat($pool, (int) ceil($length / strlen($pool)))
-            ), 0, $length);
+        /**
+         * Clean the given string.
+         *
+         * @param  string  $text
+         * @return string
+         */
+        Str::macro('cleanString', function (string $text) {
+            $text = preg_replace("/[\r\n]+/", ' ', $text);
+            $text = preg_replace("/[ ]+/", ' ', $text);
+
+            return $text;
         });
 
-        Request::macro('removeIfNull', function ($keys) {
-            $keys = is_array($keys) ? $keys : func_get_args();
+        /**
+         * Remove the keys if they're null.
+         *
+         * @param  mixed  $keys,...
+         * @return \Illuminate\Http\Request
+         */
+        Request::macro('removeIfNull', function (...$keys) {
             $keys = array_filter($keys, function ($key) {
                 return is_null($this->get($key));
             });
@@ -106,6 +149,12 @@ class MacroServiceProvider extends ServiceProvider
             return $this;
         });
 
+        /**
+         * Get next url from request.
+         *
+         * @param  string  $default
+         * @return string
+         */
         Request::macro('getNextUrl', function ($default = '/') {
             $intended = $this->session->pull('url.intended');
             $next = $this->get('next');
@@ -131,6 +180,15 @@ class MacroServiceProvider extends ServiceProvider
             return $path ? $path : $default;
         });
 
+        /**
+         * Redirect to the next url.
+         *
+         * @param  string  $default
+         * @param  int  $status
+         * @param  array  $headers
+         * @param  bool|null  $secure
+         * @return \Illuminate\Http\RedirectResponse
+         */
         Redirector::macro('next', function (
             $default = '/',
             $status = 302,
@@ -142,6 +200,16 @@ class MacroServiceProvider extends ServiceProvider
             return $this->to($path, $status, $headers, $secure);
         });
 
+        /**
+         * Get the URL to the main site for a given route instance.
+         *
+         * @param  \Illuminate\Routing\Route  $route
+         * @param  mixed  $parameters
+         * @param  bool  $absolute
+         * @return string
+         *
+         * @throws \Illuminate\Routing\Exceptions\UrlGenerationException
+         */
         UrlGenerator::macro('mainRoute', function ($name, $parameters = [], $absolute = true) {
             $name = 'main.' . $name;
 
@@ -152,6 +220,16 @@ class MacroServiceProvider extends ServiceProvider
             throw new RouteNotFoundException("Route [{$name}] not defined.");
         });
 
+        /**
+         * Get the URL to the sub site for a given route instance.
+         *
+         * @param  \Illuminate\Routing\Route  $route
+         * @param  mixed  $parameters
+         * @param  bool  $absolute
+         * @return string
+         *
+         * @throws \Illuminate\Routing\Exceptions\UrlGenerationException
+         */
         UrlGenerator::macro('subRoute', function ($name, $parameters = [], $absolute = true) {
             $name = 'sub.' . $name;
 
@@ -180,10 +258,20 @@ class MacroServiceProvider extends ServiceProvider
             throw new RouteNotFoundException("Route [{$name}] not defined.");
         });
 
+        /**
+         * Display next field.
+         *
+         * @return string
+         */
         Blade::directive('next', function () {
             return '<?php echo next_field(); ?>';
         });
 
+        /**
+         * Get DataTable attributes.
+         *
+         * @return array
+         */
         DataTableBuilder::macro('getAttributes', function () {
             return $this->attributes;
         });
