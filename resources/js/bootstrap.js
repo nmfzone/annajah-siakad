c0b48f279ace68d6c2b2877988c2b91f1327a26c
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2021-02-01 18:07:56 +0700

    chore: Use ES6 syntax

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,19 +1,15 @@
-window._ = require('lodash');
+import lodash from 'lodash'
+import jQuery from 'jquery'
+import axios from 'axios'
 
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+window._ = lodash
+window.$ = window.jQuery = jQuery
 
-window.axios = require('axios');
+window.axios = axios
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.withCredentials = true
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
