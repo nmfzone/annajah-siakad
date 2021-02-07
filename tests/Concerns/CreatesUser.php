495da6d19c76da2e89eac0ab7bdcef2c41f61312
Nabil Muhammad Firdaus <nmfdev@gmail.com>
2021-02-07 23:33:40 +0700

    chore(test): Add createStudentFor

--- tests/Concerns/CreatesUser.php
+++ tests/Concerns/CreatesUser.php
@@ -4,6 +4,7 @@ namespace Tests\Concerns;
 
 use App\Enums\Role;
 use App\Models\Site;
+use App\Models\Student;
 use App\Models\User;
 
 trait CreatesUser
@@ -18,6 +19,27 @@ trait CreatesUser
         $user->save();
 
         $site->users()->save($user);
+
+        return $user;
+    }
+
+    public function createStudentFor(Site $site, $attributes = [], $userAttributes = []): User
+    {
+        $user = User::factory()->create(array_merge([
+            'password' => bcrypt('12345678'),
+            'role' => Role::STUDENT,
+        ], $userAttributes));
+
+        $site->users()->save($user);
+
+        $user->studentProfiles()->save(
+            Student::factory()->make(array_merge([
+                'nis' => Student::generateNis($this->site),
+                'accepted_at' => now(),
+                'graduated_at' => null,
+            ], $attributes)),
+            ['site_id' => $site->id]
+        );
 
         return $user;
     }
