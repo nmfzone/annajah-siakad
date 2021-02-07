6fe619a9d30ce590823e3595ecf70f294f4154c5
Nabil Muhammad Firdaus <nmfdev@gmail.com>
2021-02-07 23:32:04 +0700

    chore: Add getFullSubUrl

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\Site;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getFullSubUrl(Site $site, $uri): string
+    {
+        $host = config('app.protocol') . '://' . $site->domain;
+
+        if ($this->app->make('url')->isValidUrl($uri)) {
+            return $uri;
+        }
+
+        return join_url($host, $uri);
+    }
 }
