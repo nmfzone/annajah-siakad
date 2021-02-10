829d5bf3b7533f141b2d3270a0520816c1e49744
Nabil Muhammad Firdaus <nmfdev@gmail.com>
2021-02-11 03:42:37 +0700

    chore: Save full path and conversion path for all disk

--- app/Listeners/MediaLibrary/SaveConversionPath.php
+++ app/Listeners/MediaLibrary/SaveConversionPath.php
@@ -2,25 +2,19 @@
 
 namespace App\Listeners\MediaLibrary;
 
-use App\Garages\GoogleDrive\GoogleDriveAdapter;
-use Illuminate\Support\Facades\Storage;
 use Spatie\MediaLibrary\Conversions\Events\ConversionHasBeenCompleted as Event;
 
 class SaveConversionPath
 {
     public function handle(Event $event)
     {
-        $conversionDiskName = $event->media->conversions_disk ?? $event->media->disk;
-        $storage = Storage::disk($conversionDiskName);
+        $lastPath = $event->media->getCustomProperty('last_conversions_path');
+        $conversionPaths = $event->media->getCustomProperty('conversion_paths', []);
 
-        if ($storage->getDriver()->getAdapter() instanceof GoogleDriveAdapter) {
-            $lastPath = $event->media->getCustomProperty('last_conversions_path');
-            $conversionPaths = $event->media->getCustomProperty('conversion_paths', []);
+        $conversionPaths[$event->conversion->getName()] = $lastPath;
 
-            $conversionPaths[$event->conversion->getName()] = $lastPath;
-
-            $event->media->setCustomProperty('conversion_paths', $conversionPaths);
-            $event->media->save();
-        }
+        $event->media->setCustomProperty('conversion_paths', $conversionPaths);
+        $event->media->forgetCustomProperty('last_conversions_path');
+        $event->media->save();
     }
 }
--- app/Listeners/MediaLibrary/SaveFullPath.php
+++ app/Listeners/MediaLibrary/SaveFullPath.php
@@ -2,22 +2,17 @@
 
 namespace App\Listeners\MediaLibrary;
 
-use App\Garages\GoogleDrive\GoogleDriveAdapter;
-use Illuminate\Support\Facades\Storage;
 use Spatie\MediaLibrary\MediaCollections\Events\MediaHasBeenAdded as Event;
 
 class SaveFullPath
 {
     public function handle(Event $event)
     {
-        $storage = Storage::disk($event->media->disk);
-
-        if ($storage->getDriver()->getAdapter() instanceof GoogleDriveAdapter) {
-            $event->media->setCustomProperty(
-                'full_path',
-                $event->media->getCustomProperty('last_full_path')
-            );
-            $event->media->save();
-        }
+        $event->media->setCustomProperty(
+            'full_path',
+            $event->media->getCustomProperty('last_full_path')
+        );
+        $event->media->forgetCustomProperty('last_full_path');
+        $event->media->save();
     }
 }
